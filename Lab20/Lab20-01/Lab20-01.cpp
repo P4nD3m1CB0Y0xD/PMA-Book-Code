da7c4962ddc62b7e8e83ff1cf32215fbf466bad0
Þ4ŊD³m¹©BøY <123909611+P4nD3m1CB0Y0xD@users.noreply.github.com>
2025-11-29 15:00:34 -0300

    Refactor HttpRequest to use member variable for URL
    Updated HttpRequest class to use a member variable for URL and changed the download method to a public method. Modified main function to create an instance of HttpRequest and call DownloadFile.

--- Lab20/Lab20-01/Lab20-01.cpp
+++ Lab20/Lab20-01/Lab20-01.cpp
@@ -1,20 +1,34 @@
-#include <iostream>
 #include <Windows.h>
+#include <urlmon.h>
 
 #pragma comment(lib, "Urlmon.lib")
 
 class HttpRequest
 {
 public:
-    void _DownloadFile(LPCSTR url)
-    {
-        URLDownloadToFileA(nullptr, url, "C:\\tempdownload.exe", 0, 0);
-    }
+	const char* url;
+	void DownloadFile()
+	{
+		::URLDownloadToFileA(
+			nullptr,
+			url,
+			"C:\\Users\\Public\\tempdownload.exe", // <-- New Windows versions doesn't allow an unprivileged user to write to the C:\ directory. So I use the PUBLIC just as an example.
+			0,
+			nullptr
+		);
+	}
 };
 
-int main(void)
+int WINAPI wWinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ PWSTR pCmdLine,
+	_In_ int nCmdShow
+)
 {
-    HttpRequest resquest;
-    resquest._DownloadFile("http://127.0.0.1:8000/cpp.html");
-    return 0;
+	HttpRequest* request = new HttpRequest;
+	request->url = "http://127.0.0.1:8000/cpp.html"; // <-- Open a CMD and run: "python3 -m http.server -b 127.0.0.1"
+	request->DownloadFile();
+	// delete request; // <-- Best practice. But, in the original binary, it doesn't use the delete operator.
+	return 0;
 }
